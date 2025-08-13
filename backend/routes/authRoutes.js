6b0bc0b79d07f955f7f8cbe1ca8af112e8929873
Muskan8878h <muskan8878h@gmail.com>
2025-08-13 12:01:09 +0530

    route creaed

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import { registration } from '../controller/authController.js';
+
+const authRoutes=express.Router();
+
+authRoutes.post("/registration", registration)
