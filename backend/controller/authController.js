03d6a18109da2738543a60ca3e9178fb1da4755d
Muskan8878h <muskan8878h@gmail.com>
2025-08-13 11:49:45 +0530

    created a signup/ register auth controoler

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,4 +1,8 @@
 import User from './model/userModel.js';
+import validator from 'validator';
+import bcrypt from "bcryptjs"
+import { genToken } from '../config/token.js';
+
 
 export const register=async(req,res)=>{
     try{
@@ -7,9 +11,27 @@ export const register=async(req,res)=>{
         if(existUser){
             return res.status(400).json({message:"User already exist"});
         }
-        if(validator)
+        if(!validator.isEmail(email)){
+            return res.status(400).json({message:"Entry valid Email"});
+        }
+        if(password.length<8){
+            return res.status(400).json({message:"Enter strong password of 8 length"});
+        }
+        let hashPassword=await bcrypt.hash(password,10);
+        const user=await User.create({name,email,password:hashPassword});
+
+        let token=await genToken(user._id);
+
+        res.cookie("token",token,{
+            httpOnly:true,
+            secure:false,
+            sameSite:"Strict",
+            maxAge: 7*24*60*60*1000,  //ms
+        });
+        return res.status(201).json(user);
     }
     catch(error){
-
+        console.log("signup error");
+        return res.status(500).json({message:`signup/register error${error}`});
     }
 }
