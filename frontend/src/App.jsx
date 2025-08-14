fdbeaad0f6221262d8ecbf931d071936c81fd789
Muskan8878h <muskan8878h@gmail.com>
2025-08-14 19:12:06 +0530

    registration and login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import { Route,Routes } from 'react-router-dom'
-import Registration from './pages/Registration'
+import React from 'react';
+import { Route,Routes } from 'react-router-dom';
+import Registration from './pages/Registration';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 const App = () => {
   return (
     <>
       <Routes>
+        <Route path='/' element={<Home/>}/>
         <Route path='/signup' element={<Registration/>}/>
+        <Route path='/login' element={<Login/>}/>
         
+
       </Routes>
     </>
   )
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,8 +5,8 @@ import google from '../assets/google.png';
 import { IoEyeOutline } from "react-icons/io5";
 import { FaEyeSlash } from "react-icons/fa";
 
-const Registration = () => {
-    let navigate=useNavigate();
+const Login = () => {
+  let navigate=useNavigate();
     let [show,setShow]=useState();
   return (
     <div className='w-[100vw] h-[100vh] bg-gradient-to-l from-[#141414] to-[#0c2025] text-[white] flex flex-col items-center justify-start'>
@@ -32,18 +32,18 @@ const Registration = () => {
                     <div className='w-[40%] h-[1px] bg-[#96969635]'></div>
                 </div>
                 <div className='w-[90%] h-[400px] flex flex-col items-center justify-center gap-[15px] relative'>
-                    <input type="text" className='w-[100%] h-[50px] border-[2px] border-[#96969635] backdrop:blur-sm rounded-lg shadow-lg bg-transparent placeholder-[#ffffffc7] px-[20px] font-semibold'  placeholder='User Name' required/>
+                    {/* <input type="text" className='w-[100%] h-[50px] border-[2px] border-[#96969635] backdrop:blur-sm rounded-lg shadow-lg bg-transparent placeholder-[#ffffffc7] px-[20px] font-semibold'  placeholder='User Name' required/> */}
                     <input type="text" className='w-[100%] h-[50px] border-[2px] border-[#96969635] backdrop:blur-sm rounded-lg shadow-lg bg-transparent placeholder-[#ffffffc7] px-[20px] font-semibold'  placeholder='Email' required/>
                     <input type={show ? "text" : "password"} className='w-[100%] h-[50px] border-[2px] border-[#96969635] backdrop:blur-sm rounded-lg shadow-lg bg-transparent placeholder-[#ffffffc7] px-[20px] font-semibold'  placeholder='Password' required/> 
-                    {!show && <FaEyeSlash className='w-[20px] h-[20px] cursor-pointer absolute right-[5%]' onClick={()=>setShow(prev=>!prev)}/>}
-                    {show && <IoEyeOutline  className='w-[20px] h-[20px] cursor-pointer absolute right-[5%]' onClick={()=>setShow(prev=>!prev)}/>}
-                    <button className='w-[100%] h-[50px] bg-[#6060f5] rounded-lg flex items-center justify-center mt-[20px] text-[17px] font-semibold'>Create Account</button>
-                    <p className='flex gap[10px]'>Do you have Account?<span className='text-[#5555f6cf] text-[17px] font-semibold cursor-pointer' onClick={()=>navigate("/login")} >Login</span></p>
+                    {!show && <FaEyeSlash className='w-[20px] h-[20px] cursor-pointer absolute right-[5%] bottom-[55%]' onClick={()=>setShow(prev=>!prev)}/>}
+                    {show && <IoEyeOutline  className='w-[20px] h-[20px] cursor-pointer absolute right-[5%] bottom-[55%]' onClick={()=>setShow(prev=>!prev)}/>}
+                    <button className='w-[100%] h-[50px] bg-[#6060f5] rounded-lg flex items-center justify-center mt-[20px] text-[17px] font-semibold'>Login</button>
+                    <p className='flex gap[10px]'>Don't have Account?<span className='text-[#5555f6cf] text-[17px] font-semibold cursor-pointer' onClick={()=>navigate("/signup")} >Create new <Accout></Accout></span></p>
                 </div>
-            </form>
+            </form> 
         </div>
     </div>
   )
 }
 
-export default Registration
+export default Login
